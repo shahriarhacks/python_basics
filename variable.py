6490730aa7f6e2fd760b3d0c75649728d1312e88
Shahriar Ahmed <ourshahriar@gmail.com>
2023-09-28 16:40:14 +0600

    Added

---
+++ variable.py
@@ -0,0 +1,16 @@
+age=45
+name='Rimi my Love for pawa kisu'
+is_single=True
+district='Rangpur'
+
+# For Single Line Comment 
+# For Single Line Comment : Ctrl+/
+
+"""  DocString or Multi Line Comment """
+# For docString or Multi Line Comment: Alt+Shift+A
+
+
+print(name)
+print(district)
+print(is_single)
+print(district)
